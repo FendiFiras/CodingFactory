c00dedad89d34984f8296434594e099b0d7fbe95
khalil1702 <khalil.chekili@esprit.tn>
2025-04-13 19:30:02 +0100

    committ

---
+++ MicroServiceEvent/src/main/java/com/example/microserviceevent/MicroServiceEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceEventApplication.class, args);
+	}
+
+}
